a9afdd535dcc81c8458b48bd3078ef8cc880600f
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-05-24 18:51:20 +0800

    Add functionality to track product names
    Prevents repeated scrapping of the same review

--- scrapper/src/websites/Scrapper.py
+++ scrapper/src/websites/Scrapper.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from bisect import insort
+from bisect import bisect_left
 
 class Scrapper:
     # A parent class for all scrappers
@@ -7,14 +9,25 @@ class Scrapper:
         self.firefox_options = webdriver.FirefoxOptions()
         self.firefox_options.add_argument("-headless")
         self.firefox_options.set_preference("browser.privatebrowsing.autostart", True)
+        self.names: list
     
-    def start(self, url = None):
+    # *_name keeps track list of product names to prevent repeated scrapping of the same product
+    def reset_names(self):
+        self.names = []
+        
+    def insert_name(self, name: str):
+        insort(self.names, name)
+    
+    def check_name(self, name: str) -> bool:
+        i = bisect_left(self.names, name)
+        return i != len(self.names) and self.names[i] == name
+        
+    def start(self, url:str = None) -> webdriver:
         driver = webdriver.Firefox(self.firefox_options)
-        driver.set_window_size(1980, 1080)
+        # driver.set_window_size(1980, 1080)
         driver.get(url)
-        driver.implicitly_wait(2)
+        # driver.implicitly_wait(1)
         return driver
-
     
-    def end(self, webdriver):
+    def end(self, webdriver:webdriver):
         webdriver.quit()
