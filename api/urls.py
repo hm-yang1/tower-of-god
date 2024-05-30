7b77a1383642897f435f3b9384d350223cdb47e4
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-05-30 20:49:44 +0800

    Updated urls.py for new views

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views.user_views import LoginView, LogoutView, RegisterView
+from .views.category_view import CategoryView
+from .views.product_views import (
+    EarbudsViewSet, 
+    HeadphonesViewSet,
+    KeyboardViewSet,
+    LaptopViewSet,
+    MouseViewSet,
+)
+
+# router to add product view sets
+router = DefaultRouter()
+router.register(r'earbuds', EarbudsViewSet)
+router.register(r'headphones', HeadphonesViewSet)
+router.register(r'keyboard', KeyboardViewSet)
+router.register(r'laptop', LaptopViewSet)
+router.register(r'mouse', MouseViewSet)
 
 urlpatterns = [
+    # Authentication Urls
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
-    path('logout/', LogoutView.as_view())
+    path('logout/', LogoutView.as_view()),
+    
+    # Get URLs
+    path('categories/', CategoryView.as_view()),
+    path('products/', include(router.urls))
 ]
