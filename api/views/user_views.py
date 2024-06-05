2c61b8772ad99b25c2e126d28dade29b75fa439e
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-06-05 20:32:31 +0800

    Added csrf_exempt to fix crsf problem in register and view

--- api/views/user_views.py
+++ api/views/user_views.py
@@ -5,12 +5,15 @@ from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.core.mail import send_mail
 from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.response import Response
 from rest_framework import permissions, status
 from ..serial.user_serializer import UserRegisterSerializer, UserSerializer
 
+@method_decorator(csrf_exempt, name='dispatch')
 class RegisterView(APIView):
     permission_classes = [permissions.AllowAny]
     
@@ -24,6 +27,7 @@ class RegisterView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@method_decorator(csrf_exempt, name='dispatch')
 class LoginView(APIView):
     permission_classes = [permissions.AllowAny]
                 
