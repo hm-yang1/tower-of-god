d687d98bc38d59bb859e8fa49d3ae712c958e675
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-06-14 04:51:19 +0800

    Updated notebookcheck_scrapper. Related to #2
    - Scrapper kinda buggy due to slow website loading.
    - Added exception handling to prevent complete breakdown when something fails to load

--- scrapper/notebookcheck_scrapper.py
+++ scrapper/notebookcheck_scrapper.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.relative_locator import locate_with
 from multiprocessing.pool import ThreadPool
+from multiprocessing import Pool
 from rapidfuzz import fuzz
 from random import sample
 from Scrapper import Scrapper
@@ -29,10 +30,10 @@ class notebookcheck_scrapper(Scrapper):
         ]
     
     def get_laptops(self) -> list:
-        return self.get_products('laptop', 90, 10)
+        return self.get_products('laptop', 85, 10)
     
     def get_phones(self) -> list:
-        return self.get_products('phone', 88, 10)
+        return self.get_products('phone', 85, 10)
     
     def get_tablets(self) -> list:
         return self.get_products('tablet', 85, 10)
@@ -76,10 +77,13 @@ class notebookcheck_scrapper(Scrapper):
         print(str(urls))
         print(len(urls))
         
+        self.end(driver)
+                
         # Concurrency for parse review
+        # Keeps breaking for some god damn reason
         products = []
         curried_parse_review = lambda x: self.parse_review(category, x)
-        pool = ThreadPool()
+        pool = ThreadPool(6)
         results = pool.map(curried_parse_review, urls)
         products.extend(results)
         pool.close()
@@ -88,80 +92,96 @@ class notebookcheck_scrapper(Scrapper):
         #     product = self.parse_review(category, url)
         #     products.append(product)
         
+        for product in products:
+            print(product.__str__())
         return products
             
     def parse_review(self, category, url):
-        driver = self.start(url)
+        # Try except block needed cause this website gives errors too frequently
+        # Might even be good practice to add to other scrapper
+        driver = None
+        product = None
+        try: 
+            driver = self.start(url)
+            driver.implicitly_wait(5)
+            
+            # Get full article
+            article = driver.find_element(By.ID, 'content')
+                        
+            # Parse specs
+            specs_whole = article.find_elements(By.CLASS_NAME, 'specs_whole')
+            if not specs_whole: 
+                print('No product. url: ' + url)
+                self.end(driver)
+                return product
+            
+            # Parse name
+            name_string = specs_whole[0].find_element(By.CLASS_NAME, 'specs_header').text
+            name = re.sub(r'\(.*?\)', '', name_string).strip()
+            
+            # Instantiate new product
+            Product = self.categories[category]
+            product = Product(name = name)
+            product.add_brand(name.split()[0])
+            product.add_review(url)
+            
+            # Parse rest of specs
+            specs_elements = specs_whole[0].find_elements(By.CLASS_NAME, 'specs_element')
+            specs = {}
+            if specs_elements:
+                for spec_element in specs_elements:
+                    keys = spec_element.find_elements(By.CLASS_NAME, 'specs')
+                    if len(keys) < 1: continue
+                    key = keys[0]
+                    value = spec_element.find_element(By.CLASS_NAME, 'specs_details')
+                    
+                    # Check if got link
+                    links = value.find_elements(By.TAG_NAME, 'a')
+                    if len(links) > 0:
+                        specs[key.text] = links[0].text
+                        continue
+                    
+                    specs[key.text] = value.text
+            
+            # Get pros and cons
+            pros_and_cons = article.find_elements(By.CLASS_NAME, 'ttcl_55.csc-default.csc-space-before-5.csc-space-after-15')[-1]
+
+            # Get pros
+            pros = pros_and_cons.find_element(By.CLASS_NAME, 'pc_element.pc_element_pro').find_elements(By.CLASS_NAME, 'pc_tr')
+            for pro in pros:
+                product.add_pros(pro.text[2:])
+            
+            # Get cons
+            cons = pros_and_cons.find_element(By.CLASS_NAME, 'pc_element.pc_element_contra').find_elements(By.CLASS_NAME, 'pc_tr')
+            for con in cons:
+                product.add_cons(con.text[2:])
+            
+            # Get description
+            verdict = article.find_elements(By.CLASS_NAME, 'ttcl_0.csc-default')
+            paragraphs = verdict[0].find_elements(By.TAG_NAME, 'p') + verdict[-6].find_elements(By.TAG_NAME, 'p')
+            for paragraph in paragraphs:
+                product.add_description(paragraph.text)
+            
+            # Skip addition parsing if no specs_elements
+            if not specs_elements: return product
+            
+            # Additional parsing for specific category
+            match category:
+                case 'laptop':
+                    product = self.parse_laptop(product, specs, article)
+                case 'phone':
+                    product = self.parse_phone(product, specs, article)
+                case 'tablet':
+                    product = self.parse_tablet(product, specs, article)
         
-        # Get full article
-        article = driver.find_element(By.ID, 'content')
+        except Exception as e:
+            print(e)
+            print(url)
         
-        print(url)
-        
-        # Parse specs
-        specs_whole = article.find_elements(By.CLASS_NAME, 'specs_whole')
-        if not specs_whole: 
-            print('No product' + url)
-            return None
-        
-        # Parse name
-        name_string = specs_whole[0].find_element(By.CLASS_NAME, 'specs_header').text
-        name = re.sub(r'\(.*?\)', '', name_string).strip()
-        # Instantiate new product
-        Product = self.categories[category]
-        product = Product(name = name)
-        product.add_brand(name.split()[0])
-        product.add_review(url)
-        
-        # Parse rest of specs
-        specs_elements = specs_whole.find_elements(By.CLASS_NAME, 'specs_element')
-        specs = {}
-        if specs_elements:
-            for spec_element in specs_elements:
-                keys = spec_element.find_elements(By.CLASS_NAME, 'specs')
-                if len(keys) < 1: continue
-                key = keys[0]
-                value = spec_element.find_element(By.CLASS_NAME, 'specs_details')
-                
-                # Check if got link
-                links = value.find_elements(By.TAG_NAME, 'a')
-                if len(links) > 0:
-                    specs[key.text] = links[0].text
-                    continue
-                
-                specs[key.text] = value.text
-        
-        # Get pros and cons
-        pros_and_cons = article.find_elements(By.CLASS_NAME, 'ttcl_55.csc-default.csc-space-before-5.csc-space-after-15')[-1]
-        
-        # Get pros
-        pros = pros_and_cons.find_element(By.CLASS_NAME, 'pc_element.pc_element_pro').find_elements(By.CLASS_NAME, 'pc_tr')
-        for pro in pros:
-            product.add_pros(pro.text[2:])
-        
-        # Get cons
-        cons = pros_and_cons.find_element(By.CLASS_NAME, 'pc_element.pc_element_contra').find_elements(By.CLASS_NAME, 'pc_tr')
-        for con in cons:
-            product.add_cons(con.text[2:])
-        
-        # Get conclusion
-        verdict = article.find_elements(locate_with(By.CLASS_NAME, 'ttcl_0.csc-default').below({By.CLASS_NAME: 'pc_whole.tx-nbc2fe-incontent-column'}))
-        product.add_description(verdict.text)
-        
-        # Skip addition parsing if no specs_elements
-        if not specs_elements: return product
-        
-        # Additional parsing for specific category
-        match category:
-            case 'laptop':
-                product = self.parse_laptop(product, specs, article)
-            case 'phone':
-                product = self.parse_phone(product, specs, article)
-            case 'tablet':
-                product = self.parse_tablet(product, specs, article)
-        
-        print(product.__str__())
-        return product
+        finally:
+            if driver is not None:
+                driver = self.end(driver)
+            return product
         
     def parse_laptop(self, product, specs, article):
         product = self.parse_phone(product, specs, article)
@@ -196,8 +216,9 @@ class notebookcheck_scrapper(Scrapper):
 
 def main():
     scrapper = notebookcheck_scrapper()
-    scrapper.get_phones()
-    # scrapper.get_laptops()
+    # scrapper.get_phones()
+    scrapper.get_laptops()
+    # scrapper.parse_review('phone', 'https://www.notebookcheck.net/Google-Pixel-8-smartphone-review-Compact-and-with-7-years-of-updates.768436.0.html')
     
     # Models not implemented yet
     # scrapper.get_tablets()
