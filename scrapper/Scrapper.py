cd49162322bc2ff139589163ecb15925a0719c42
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-06-16 21:59:04 +0800

    Add price scrapping in Scrapper.py. Related to #2
    - Scrapes Amazon.sg website.
    - Works by searching the product name then scraping first non sponsored result
    - Works well, even works with amazon US
    - Will give bad results if amazon does not have the product. Might be mitigated by setting realistic prices for each category of product

--- scrapper/Scrapper.py
+++ scrapper/Scrapper.py
@@ -6,6 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
 import api.models as model
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bisect import insort
 from bisect import bisect_left
 
@@ -21,6 +22,9 @@ class Scrapper:
         'laptop': model.Laptop,
         'mouse': model.Mouse,
         'phone': model.Phone,
+        'television': model.Television,
+        'monitor': model.Monitor,
+        'speaker': model.Speaker,
     }
         
     def __init__(self):
@@ -28,6 +32,28 @@ class Scrapper:
         self.firefox_options.add_argument("-headless")
         self.firefox_options.set_preference("browser.privatebrowsing.autostart", True)
         self.names: list[str]
+    
+    def get_price(self, product):
+        amazon_url = 'https://www.amazon.sg/s?k=' + product.name
+        driver = self.start(amazon_url)
+        
+        # Need additional waiting cause amazon special, might block if too fast
+        driver.implicitly_wait(5)
+        
+        # Get price of product
+        search_results = driver.find_elements(By.CSS_SELECTOR, '[data-component-type="s-search-result"]')
+        for search_result in search_results:
+            # Check if sponsored post
+            if len(search_result.find_elements(By.CLASS_NAME, 'puis-label-popover.puis-sponsored-label-text')) > 0: continue
+            
+            possible_prices = search_result.find_elements(By.CLASS_NAME, 'a-price-whole')
+            if len(possible_prices) > 0: 
+                price = possible_prices[0].text.replace(',','')
+                product.add_price(float(price))
+                break
+        
+        self.end(driver)
+        return product
     
     # *_name keeps track list of product names to prevent repeated scrapping of the same product
     def reset_names(self):
