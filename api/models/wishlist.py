3f165a0f6765217288550148484f0c3a83bba8a5
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-06-19 22:10:28 +0800

    Created wishlist. Related to #1

---
+++ api/models/wishlist.py
@@ -0,0 +1,61 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from .user import User
+from .category import Category
+from rest_framework import serializers
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from ..serial.user_serializer import UserSerializer
+from .earbuds import Earbuds, EarbudSerializer
+from .keyboard import Keyboard, KeyboardSerializer
+from .laptop import Laptop, LaptopSerializer
+from .mouse import Mouse, MouseSerializer
+from .phone import Phone, PhoneSerializer
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='search_histories')
+    
+    # Using content types to identify product
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    
+    datetime = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        result = self.user.username 
+        result +=  '\n' + str(self.content_type)
+        result += '\n' + str(self.content_object)
+        result += '\n' + str(self.datetime)
+        return result
+    
+class WishlistSerializer(serializers.ModelSerializer):
+    # Serializes user
+    user = UserSerializer()
+    
+    # Serializes product
+    content_object = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Wishlist
+        fields = '__all__'
+        
+    def get_content_object(self, obj):
+        content_type = obj.content_type
+        model_class = content_type.model_class()
+                
+        if model_class == Earbuds:
+            serializer_class = EarbudSerializer
+        elif model_class == Keyboard:
+            serializer_class = KeyboardSerializer
+        elif model_class == Laptop:
+            serializer_class = LaptopSerializer
+        elif model_class == Mouse:
+            serializer_class = MouseSerializer
+        elif model_class == Phone:
+            serializer_class = PhoneSerializer
+        else:
+            serializer_class = serializers.Serializer
+
+        serializer = serializer_class(instance=obj.content_object, context=self.context)
+        return serializer.data
---
+++ api/views/wishlist_views.py
@@ -0,0 +1,98 @@
+import os
+from django.contrib.auth import authenticate
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.contenttypes.models import ContentType
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from ..serial.user_serializer import UserSerializer
+from ..models.wishlist import Wishlist, WishlistSerializer
+from ..models.earbuds import Earbuds, EarbudSerializer
+from ..models.keyboard import Keyboard, KeyboardSerializer
+from ..models.laptop import Laptop, LaptopSerializer
+from ..models.mouse import Mouse, MouseSerializer
+from ..models.phone import Phone, PhoneSerializer
+
+# Wishlist views
+
+# View to get wishlist of user that sent the request
+class WishlistViewset(ModelViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    serializer_class = WishlistSerializer
+    
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Wishlist.objects.filter(user = user).order_by('-datetime')        
+        return queryset
+    
+    def create(self, request, *args, **kwargs):
+        product_category = request.data.get('product_category')
+        object_id = request.data.get('object_id')
+        
+        if not product_category or not object_id:
+            return Response({"error": "Product category and object ID are required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Determine the model class
+        match product_category:
+            case 'earbuds':
+                product = Earbuds
+            case 'keyboard':
+                product = Keyboard
+            case 'laptop':
+                product = Laptop
+            case 'mouse':
+                product = Mouse
+            case 'phone':
+                product = Phone
+            case 'television':
+                product = None
+            case 'monitor':
+                product = None
+            case 'speaker':
+                product = None
+            case _:
+                return Response({"error": "Invalid product category"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if the object_id exists in the specified model
+        try:
+            product_instance = product.objects.get(id=object_id)
+        except product.DoesNotExist:
+            return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        # Get content type
+        content_type = ContentType.objects.get_for_model(product_instance)
+        
+        # Check if item already exist in wishlist
+        queryset = self.get_queryset()
+        if queryset.filter(content_type=content_type, object_id=object_id).exists():
+            return Response({'error': 'Item already exists in your wishlist'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Create a wishlist item
+        wishlist_item = Wishlist.objects.create(
+            user=request.user,
+            content_type=ContentType.objects.get_for_model(product_instance),
+            object_id=product_instance.id,
+        )
+        
+        print(str(wishlist_item))
+        
+        return Response({"message": "Wishlist item created successfully"}, status=status.HTTP_201_CREATED)
+    
+    # No need for custom delete given model viewset provides default implementation
+    # def destroy(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+        
+    #     queryset = self.get_queryset()
+    #     wish = queryset.filter(id=instance.id)
+    #     print(wish)
+    #     if not wish.exists():
+    #         return Response({"error": "Wish does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+    #     wish.delete()
+    #     return Response({"message": "Wishlist item deleted successfully"}, status=status.HTTP_200_OK)
