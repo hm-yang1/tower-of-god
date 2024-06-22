b6f1125fc3e5368167fee736a380f6db00a5c1df
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-06-22 22:38:26 +0800

    Changed static url

--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-#  Blah blahs blah -main
 import os
 from django.conf import settings
 from dotenv import load_dotenv
@@ -236,7 +235,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
