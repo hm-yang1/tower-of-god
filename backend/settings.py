40ade4a870c9bc8995ae1f00a6d5ed4b29f9f047
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-06-22 21:27:32 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+# I have updated some stuff on main
+
 import os
 from django.conf import settings
 from dotenv import load_dotenv
