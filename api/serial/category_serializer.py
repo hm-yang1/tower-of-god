7ab5651f4e18bbc882818426effe9179a02c0c37
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-06-23 21:22:21 +0800

    Delete category_serializer.py

--- api/serial/category_serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from ..models.category import Category
-
-class CatergorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
