a130bdd7e7ca0c1f03ba6742e0f5fbbb9bee65e1
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-06-27 21:28:13 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -56,9 +56,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    
-    'DEFAULT_FILTER_BACKENDS': 'django_filters.rest_framework.DjangoFilterBackend',
-    
+        
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20,
     
