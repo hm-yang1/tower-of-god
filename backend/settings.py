8f84b9e832d0d697bea2e4e55cd5a9b335e6a86b
hm-yang1 <153845481+hm-yang1@users.noreply.github.com>
2024-06-28 22:39:49 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -58,7 +58,7 @@ REST_FRAMEWORK = {
     ],
         
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 12,
     
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
