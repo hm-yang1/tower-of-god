d6f05657be575beb882c095ab4818362aded6449
Betahaxer <108223321+Betahaxer@users.noreply.github.com>
2024-07-02 17:11:45 +0800

    Update urls.py
    Added token refresh view to enable token refresh

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,7 @@ from .views.wishlist_views import WishlistViewSet
 from .views.user_views import LoginView, LogoutView, RegisterView
 from .views.product_views import ProductViewSet
 from .views.comparison_views import ComparisonViewSet
+from rest_framework_simplejwt.views import TokenRefreshView
 
 # router to add product view sets
 router = DefaultRouter()
@@ -19,6 +20,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     # Get URLs
     path('', include(router.urls)),
